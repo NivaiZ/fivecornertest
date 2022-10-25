ff4ba2509ff677a8fab9b202b4af38b51349125d
ilya <nivaiz54@gmail.com>
2022-10-25 23:00:06 +0500

    завершение проекта

--- source/js/index.js
+++ source/js/index.js
@@ -108,7 +108,7 @@ var phoneMask = IMask(
 const element = document.querySelector('.form-order__select');
 const choices = new Choices(element, {
   searchEnabled: false,
-  itemSelectText: 'Выберите нужную упаковку',
+  itemSelectText: false,
 });
 
 
@@ -130,10 +130,12 @@ orderForm.addEventListener('submit', retrieveFormValue)
 const clickBurgerMenuFunction = () => {
   const burgerButton = document.querySelector(".header-block__burger");
   const contentMenu = document.querySelector('.header-block__content');
+  const bodyClass = document.querySelector('.page-body')
 
   burgerButton.addEventListener('click', () => {
     burgerButton.classList.toggle('header-block__burger--open');
     contentMenu.classList.toggle('header-block__content--open');
+    bodyClass.classList.toggle('page-body__noscroll');
   })
 
   window.addEventListener('click', e => {
@@ -141,6 +143,7 @@ const clickBurgerMenuFunction = () => {
     if (!target.closest('.header-block__content') && !target.closest('.header-block__burger')) {
       contentMenu.classList.remove('header-block__content--open');
       burgerButton.classList.remove('header-block__burger--open');
+      bodyClass.classList.remove('page-body__noscroll');
     }
   })
 
@@ -150,10 +153,46 @@ const clickBurgerMenuFunction = () => {
         evt.preventDefault();
         contentMenu.classList.remove('header-block__content--open');
         burgerButton.classList.remove('header-block__burger--open');
+        bodyClass.classList.remove('page-body__noscroll');
       }
     }
   });
 
 }
+
+// search
+
+const searchButton = document.querySelector('.search-header__item');
+const inputSearch = document.querySelector('.search-header__enter');
+const addClassOverlay = document.querySelector('.search-header__modal');
+const buttonSubmit = document.querySelector('.search-header__burger');
+
+
+searchButton.addEventListener('click', (e) => {
+  inputSearch.focus();
+  inputSearch.classList.add('search-header__enter--open');
+  addClassOverlay.classList.add('search-header__modal--open');
+  buttonSubmit.classList.add('search-header__burger--open');
+
+})
+
+searchButton.addEventListener('keydown', (e) => {
+  if (e.keyCode === 27) {
+    if (addClassOverlay.classList.contains('search-header__modal--open')) {
+      addClassOverlay.classList.remove('search-header__modal--open');
+      inputSearch.classList.remove('search-header__enter--open');
+      buttonSubmit.classList.remove('search-header__burger--open');
+    }
+  }
+})
+
+addClassOverlay.addEventListener('click', (e) => {
+  const target = e.target;
+  if (!target.closest('.header-block__content') && !target.closest('.header-block__burger')) {
+    addClassOverlay.classList.remove('search-header__modal--open');
+    inputSearch.classList.remove('search-header__enter--open');
+    buttonSubmit.classList.remove('search-header__burger--open');
+  }
+})
 
 clickBurgerMenuFunction();
